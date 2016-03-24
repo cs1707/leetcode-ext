0187d18070e868f88b703bf8bf27f9db9fe8dfca
Xiakun Lu <luxiakun@gmail.com>
2016-03-24 12:19:55 -0400

    add countdown for run code button and submit button

---
+++ leetcode-ext/leetcode-countdown.js
@@ -0,0 +1,27 @@
+/**
+ * Created by binarylu on 3/24/16.
+ */
+
+$(function () {
+    // run code button
+    document.getElementById("button0").onclick=function(){show_time_count(this);};
+    // submit button
+    document.getElementById("button1").onclick=function(){show_time_count(this);};
+});
+
+var wait_time = 10;
+function show_time_count(obj) {
+    var title = $(obj).html().replace(/ \(\d+\)/, "");
+    if (wait_time === 0) {
+        $("#button0, #button1").attr("disabled", false);
+        $(obj).html(title);
+        wait_time = 10;
+    } else {
+        $("#button0, #button1").attr("disabled", true);
+        wait_time--;
+        $(obj).html(title + " (" + wait_time + ")");
+        setTimeout(function() {
+            show_time_count(obj);
+        }, 1000);
+    }
+}
