cb7341995f95c7cbd677b571a916e0dbcd2d5ec8
Xiakun Lu <luxiakun@gmail.com>
2016-04-23 23:32:18 -0400

    v1.0.5

--- server/app.js
+++ server/app.js
@@ -126,7 +126,7 @@ function save_problem(req, res) {
     var tags = data.tags;
     var similarities = data.similarities;
 
-    if (data.contributor.version !== '1.0.4') {
+    if (data.contributor.version !== '1.0.5') {
         res.send({"res": "version is out of date"});
         return;
     }
