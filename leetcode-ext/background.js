b0d6450a3c7fc0d2c49b1a538a91e0372b2cb6c7
Xiakun Lu <luxiakun@gmail.com>
2016-04-28 00:57:29 -0400

    add background: open option page at the first time this extension is installed

---
+++ leetcode-ext/background.js
@@ -0,0 +1,8 @@
+/**
+ * Created by binarylu on 4/28/16.
+ */
+
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason === "install")
+        chrome.tabs.create({url: "chrome://extensions/?options=" + chrome.runtime.id});
+});
