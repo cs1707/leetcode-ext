998231998bca687e7d4a75fe2a5f60f3e3556329
Xiakun Lu <luxiakun@gmail.com>
2016-04-28 01:27:55 -0400

    use jquery to add menu items

--- leetcode-ext/popup/popup.js
+++ leetcode-ext/popup/popup.js
@@ -2,26 +2,24 @@
  * Created by binarylu on 3/24/16.
  */
 
-window.onload = function() {
-    var elements = document.getElementsByTagName("tr");
+$(function() {
     chrome.storage.sync.get({
         user: '',
         repo_name: ''
     }, function(items) {
-        for (var i = 0; i < elements.length; ++i) {
-            var url = "";
-            elements[i].onclick=function(){
-                if (this.id === 'go_leetcode') {
-                    url = "https://leetcode.com/problemset/algorithms/";
-                } else if (this.id === 'go_github') {
-                    url = "https://github.com/" + items.user + "/" + items.repo_name;
-                } else if (this.id === 'go_settings') {
-                    //url = "chrome://extensions/?options=" + chrome.runtime.id;
-                    chrome.runtime.openOptionsPage();
-                    return true;
-                }
-                chrome.tabs.create({url: url});
-            };
-        }
+        $("#menu").append($('<tr id="go_leetcode"><td>Go to LeetCode.com</td></tr>'));
+        if (items.user !== "")
+            $("#menu").append($('<tr id="go_github"><td>Go to Your GitHub Repository</td></tr>'));
+        $("#menu").append($('<tr id="go_settings"><td style="border-top:1px solid lightgray;">Settings</td></tr>'));
+
+        $("#go_leetcode").click(function() {
+            chrome.tabs.create({url: "https://leetcode.com/problemset/algorithms/"});
+        });
+        $("#go_github").click(function() {
+            chrome.tabs.create({url: "https://github.com/" + items.user + "/" + items.repo_name});
+        });
+        $("#go_settings").click(function() {
+            chrome.runtime.openOptionsPage();
+        });
     });
-};
+});
