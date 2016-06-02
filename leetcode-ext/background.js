881daba1026479965051380f8858abc302454d92
Xiakun Lu <luxiakun@gmail.com>
2016-06-02 01:36:12 -0400

    Add update notification

--- leetcode-ext/background.js
+++ leetcode-ext/background.js
@@ -6,5 +6,20 @@ chrome.runtime.onInstalled.addListener(function (details) {
     if (details.reason === "install") {
         chrome.runtime.openOptionsPage();
         //chrome.tabs.create({url: "chrome://extensions/?options=" + chrome.runtime.id});
+    } else if (details.reason === "update") {
+        var currentVersion = chrome.runtime.getManifest().version;
+        var notificationId = "leetcode-ext-upgrade";
+        var notification = {
+            type: "basic",
+            title: "LeetCode Extension Upgrade",
+            message: "Upgrade to version " + currentVersion + ". \nClick here for more information.",
+            iconUrl: chrome.runtime.getURL("icons/icon128.png"),
+            isClickable: true
+        };
+        chrome.notifications.create(notificationId, notification, function(notificationId) {
+            return chrome.notifications.onClicked.addListener(function(notificationId) {
+                chrome.tabs.create({url: "https://github.com/binarylu/leetcode-ext#release-notes"});
+            });
+        });
     }
 });
